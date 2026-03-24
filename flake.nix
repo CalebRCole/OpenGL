5568fcd34f81142479cda347d481897bd3bf4968
Caleb Cole <calebrylancole@gmail.com>
2026-03-24 16:54:28 -0400

    fix: Correct OpenGL driver location
    nixGL allows proper use of OpenGL drivers

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:mitchellh/zig-overlay";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
   outputs =
@@ -13,21 +14,26 @@
       nixpkgs,
       flake-utils,
       zig,
+      nixgl,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
         zig-pkg = zig.packages.${system}.master;
+        nixGL_wrap = nixgl.packages.${system}.nixGLDefault;
       in
       {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
+            bear
+            clang-tools
             zig-pkg
             zls
             pkg-config
             gdb
             valgrind
+            nixGL_wrap
           ];
 
           buildInputs = with pkgs; [
@@ -36,14 +42,15 @@
           ];
 
           shellHook = ''
-            echo "--- Development Environment ---"
-            echo "Targeting: ${system}"
-            export LD_LIBRARY_PATH="${
-              pkgs.lib.makeLibraryPath [
-                pkgs.glfw
-                pkgs.libGL
-              ]
-            }:$LD_LIBRARY_PATH"
+                        echo "--- Development Environment ---"
+                        echo "Targeting: ${system}"
+                        export LD_LIBRARY_PATH="${
+                          pkgs.lib.makeLibraryPath [
+                            pkgs.glfw
+                            pkgs.libGL
+                          ]
+                        }:$LD_LIBRARY_PATH"
+            alias zig="${nixGL_wrap}/bin/nixGL zig"
           '';
         };
       }
